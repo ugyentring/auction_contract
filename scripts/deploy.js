a3a62b7c185ef838648cd8fe874ee4c589e9f124
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-30 11:43:39 +0600

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  // We get the contract to deploy
+  const Auction = await hre.ethers.getContractFactory("Auction");
+  const auction = await Auction.deploy("Hello, Hardhat!");
+
+  await auction.deployed();
+
+  console.log("Auction deployed to:", auction.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
