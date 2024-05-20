201b1602aa104d3db6a9f5fb955305f84a70062a
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-05-21 01:48:22 +0600

    changes

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -24,25 +24,19 @@ contract Auction {
 
     uint bidIncrement;
 
-    //the owner can finalize the auction and get the highestBindingBid only once
-
     bool public ownerFinalized = false;
 
     constructor() {
         owner = payable(msg.sender);
-
         auctionState = State.Running;
-
         startBlock = block.number;
 
-        endBlock = startBlock + 3; //40320
+        endBlock = startBlock + 3; 
 
         ipfsHash = "";
 
-        bidIncrement = 100; // bidding in multiple of ETH
+        bidIncrement = 100; 
     }
-
-    // declaring function modifiers
 
     modifier notOwner() {
         require(msg.sender != owner);
@@ -68,8 +62,6 @@ contract Auction {
         _;
     }
 
-    //a helper pure function (it neither reads, nor it writes to the blockchain)
-
     function min(uint a, uint b) internal pure returns (uint) {
         if (a <= b) {
             return a;
@@ -78,13 +70,9 @@ contract Auction {
         }
     }
 
-    // only the owner can cancel the Auction before the Auction has ended
-
     function cancelAuction() public beforeEnd onlyOwner {
         auctionState = State.Canceled;
     }
-
-    // the main function called to place a bid
 
     function placeBid()
         public
@@ -97,32 +85,17 @@ contract Auction {
         // to place a bid auction should be running
         require(auctionState == State.Running);
 
-        // minimum value allowed to be sent
-
-        // require(msg.value > 0.0001 ether);
-
         uint currentBid = bids[msg.sender] + msg.value;
-
-        // the currentBid should be greater than the highestBindingBid.
-
-        // Otherwise there's nothing to do.
-
         require(currentBid > highestBindingBid);
-
-        // updating the mapping variable
 
         bids[msg.sender] = currentBid;
 
         if (currentBid <= bids[highestBidder]) {
-            // highestBidder remains unchanged
-
             highestBindingBid = min(
                 currentBid + bidIncrement,
                 bids[highestBidder]
             );
         } else {
-            // highestBidder is another bidder
-
             highestBindingBid = min(
                 currentBid,
                 bids[highestBidder] + bidIncrement
@@ -135,49 +108,29 @@ contract Auction {
     }
 
     function finalizeAuction() public {
-        // the auction has been Canceled or Ended
-
         require(auctionState == State.Canceled || block.number > endBlock);
-
-        // only the owner or a bidder can finalize the auction
-
         require(msg.sender == owner || bids[msg.sender] > 0);
-
-        // the recipient will get the value
 
         address payable recipient;
 
         uint value;
 
         if (auctionState == State.Canceled) {
-            // auction canceled, not ended
 
             recipient = payable(msg.sender);
 
             value = bids[msg.sender];
         } else {
-            // auction ended, not canceled
-
             if (msg.sender == owner && ownerFinalized == false) {
-                //the owner finalizes the auction
-
                 recipient = owner;
-
                 value = highestBindingBid;
-
-                //the owner can finalize the auction and get the highestBindingBid only once
-
                 ownerFinalized = true;
             } else {
-                // another user (not the owner) finalizes the auction
-
                 if (msg.sender == highestBidder) {
                     recipient = highestBidder;
 
                     value = bids[highestBidder] - highestBindingBid;
                 } else {
-                    //this is neither the owner nor the highest bidder (it's a regular bidder)
-
                     recipient = payable(msg.sender);
 
                     value = bids[msg.sender];
@@ -185,12 +138,7 @@ contract Auction {
             }
         }
 
-        // resetting the bids of the recipient to avoid multiple transfers to the same recipient
-
         bids[recipient] = 0;
-
-        //sends value to the recipient
-
         recipient.transfer(value);
     }
 }
